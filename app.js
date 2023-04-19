71842b9c1381380df06f2e9613b2998be9072d5a
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2023-04-19 19:19:40 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Custom
+app.use('/', (req,res) => {
+    res.status(200).json({
+        message: 'Welcome to API Tiketing'
+    })
+})
+
+
+module.exports = app;
